85035a052c09cf37e7be07a0bd6b5811451b377a
yusuke.yanagisawa <miso_shiru0141wakame@outlook.jp>
2024-12-08 18:43:08 +0900

    refact:DB操作についてDIの導入

--- databases/database.go
+++ databases/database.go
@@ -1,9 +1,8 @@
 package databases
 
 import (
-	"github.com/willow-swamp/shopping-notifier/models"
-
 	mysql "github.com/go-sql-driver/mysql"
+	"github.com/willow-swamp/shopping-notifier/models"
 	gorm_mysql "gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -24,61 +23,7 @@ func DBConn() (*gorm.DB, error) {
 	return db, err
 }
 
-func Migrate() error {
-	db, err := DBConn()
-	if err != nil {
-		return err
-	}
-	err = db.AutoMigrate(&models.User{}, &models.Item{}, &models.Group{}, &models.Whitelist{})
+func Migrate(db *gorm.DB) error {
+	err := db.AutoMigrate(&models.User{}, &models.Item{}, &models.Group{}, &models.Whitelist{})
 	return err
-}
-
-func GetItems() ([]models.Item, error) {
-	db, err := DBConn()
-	if err != nil {
-		return nil, err
-	}
-	var items []models.Item
-	db.Find(&items)
-	return items, nil
-}
-
-func GetItem(id int) (*models.Item, error) {
-	db, err := DBConn()
-	if err != nil {
-		return nil, err
-	}
-	var item models.Item
-	result := db.First(&item, id)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-	return &item, nil
-}
-
-func CreateItem(item *models.Item) error {
-	db, err := DBConn()
-	if err != nil {
-		return err
-	}
-	db.Create(item)
-	return nil
-}
-
-func UpdateItem(item *models.Item) error {
-	db, err := DBConn()
-	if err != nil {
-		return err
-	}
-	db.Updates(item)
-	return nil
-}
-
-func DeleteItem(id int) error {
-	db, err := DBConn()
-	if err != nil {
-		return err
-	}
-	db.Delete(&models.Item{}, id)
-	return nil
 }
