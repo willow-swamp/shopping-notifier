f8c99e9d91d976732496b7ccc92c4001c484a771
yusuke.yanagisawa <miso_shiru0141wakame@outlook.jp>
2024-12-08 20:29:21 +0900

    refact:serviceインタフェースの修正

--- service/service.go
+++ service/service.go
@@ -5,30 +5,30 @@ import (
 	"github.com/willow-swamp/shopping-notifier/models"
 )
 
-type Service struct {
-	repository databases.Repository
+type ItemService struct {
+	repository databases.ItemRepository
 }
 
-func NewService(repository databases.Repository) *Service {
-	return &Service{repository: repository}
+func NewItemService(repository databases.ItemRepository) *ItemService {
+	return &ItemService{repository: repository}
 }
 
-func (s *Service) GetItems() ([]models.Item, error) {
+func (s *ItemService) GetItems() ([]models.Item, error) {
 	return s.repository.GetItems()
 }
 
-func (s *Service) GetItem(id int) (*models.Item, error) {
+func (s *ItemService) GetItem(id int) (*models.Item, error) {
 	return s.repository.GetItem(id)
 }
 
-func (s *Service) CreateItem(item *models.Item) error {
+func (s *ItemService) CreateItem(item *models.Item) error {
 	return s.repository.CreateItem(item)
 }
 
-func (s *Service) UpdateItem(item *models.Item) error {
+func (s *ItemService) UpdateItem(item *models.Item) error {
 	return s.repository.UpdateItem(item)
 }
 
-func (s *Service) DeleteItem(id int) error {
+func (s *ItemService) DeleteItem(id int) error {
 	return s.repository.DeleteItem(id)
 }
